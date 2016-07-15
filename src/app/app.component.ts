821ff4452c2521ff3664a4e8c15a048e0fb5c266
Lam Pham <phamlam92@gmail.com>
2016-07-15 17:52:00 -0500

    reset
    reset

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { ROUTER_DIRECTIVES } from '@angular/router';
+import { HeaderComponent } from './header.component';
+import { RecipeService } from './recipes'
+
+@Component({
+  moduleId: module.id,
+  selector: 'rb-root',
+  templateUrl: 'app.component.html',
+  directives: [HeaderComponent, ROUTER_DIRECTIVES],
+  providers: [RecipeService]
+})
+export class AppComponent {
+
+}
